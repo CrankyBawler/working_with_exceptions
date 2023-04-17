15e1a5a754ab167e87eefea5005f32db16f2a72a
Cranky Bawler <6666lenia6666@mail.ru>
2023-04-17 21:35:28 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
     }
-}
+    public static String authorization (String login, String password, String confirmPassword ) {
+            if (login.length() >= 20) {
+                throw new WrongLoginException
+
+            }
+
+
+          //  if (Pattern.matches("^[a-zA-Z_0-9]", test))
+        //if (!file.exists()) {
+         //   throw new FileNotFoundException();
+        }
+       // if (file.isDirectory()) {
+         //   throw new IllegalArgumentException(file.getPath() + " is a directory");
+        }
+
+
+
---
+++ src/User.java
@@ -0,0 +1,39 @@
+public class User {
+    private String login;
+    private String password;
+    private String confirmPassword;
+
+    public User(String login, String password, String confirmPassword) {
+        this.login = login;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        if (login.length() >= 20) {
+            return ;
+        }
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+}
+
