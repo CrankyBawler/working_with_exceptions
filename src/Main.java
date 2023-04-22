1f2c6636a4d1d3d5d9f6573ea91f5e8fa3051fbf
Cranky Bawler <6666lenia6666@mail.ru>
2023-04-22 12:05:12 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -6,24 +6,23 @@ public class Main {
         try {
             check("java_skypro_go", "D_1hWiKjjP_9", "D_1hWiKjjP_9");
             System.out.println("Вход выполнен успешно");
-        } catch (WrongPasswordException e){
+        } catch (WrongPasswordException e) {
             e.printStackTrace();
-        } catch (WrongLoginException e){
+        } catch (WrongLoginException e) {
             e.printStackTrace();
-        }
-        finally {
+        } finally {
             System.out.println("Добрый день!");
         }
     }
 
-    private static void check (String login, String password, String confirmPassword){
-        if (login.length()>=20 || !login.matches("[a-zA-Z0-9_]+")){
+    private static void check(String login, String password, String confirmPassword) {
+        if (login.length() >= 20 || !login.matches("[a-zA-Z0-9_]+")) {
             throw new WrongLoginException("Логин слишком длинный или содержит запрещенные символы");
         }
         if (password.length() > 20 || !password.equals(confirmPassword) || !password.matches("[a-zA-z0-9_]+"))
             throw new WrongPasswordException("Не корректный пароль");
     }
-           }
+}
 
 
 
